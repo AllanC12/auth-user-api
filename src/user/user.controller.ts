ff94ac4d0906534ed751bade130c659269d9a208
AllanC12 <allandevfront@gmail.com>
2025-03-03 09:50:44 -0300

    feat - criação de controllers para update e dto de create

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,42 @@
-import { Controller, Post, Get,Body , Param} from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
 
 @Controller('users')
 export class UserController {
-    @Post()
-    async Create(@Body() body){
-        return {body}
-    }
+  @Post()
+  async Create(@Body() body: CreateUserDTO) {
+    return { body };
+  }
 
-    @Get()
-    async GetAllUsers(){
-        return {users : []}
-    }
+  @Get()
+  async GetAllUsers() {
+    return { users: [] };
+  }
 
-    @Get(':id')
-    async GetOneUser(@Param() param){
-        return  {user :{} , param}
-    }
+  @Get(':id')
+  async GetOneUser(@Param() param) {
+    return { user: {}, param };
+  }
+
+  @Patch(':id')
+  async UpdateUser(@Body() body, @Param() params) {
+    return {
+      method: 'Patch',
+      body,
+      params,
+    };
+  }
+
+  @Delete(':id')
+  async DeleteUSer(@Param() params) {
+    return { params };
+  }
 }
